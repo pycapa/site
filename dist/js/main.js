6ab029df7b722c7da8e77cc20ccc8346d24df181
= <=>
2022-01-23 17:02:32 -0500

    final version

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,13 @@
+// menu funcionality.
+// hamburger boton.
 const mnuBtn = document.querySelector('#mnuBtn')
-//const overlay = document.querySelector('.overlay')
 const menu = document.querySelector('.navbar-mobil')
 const body = document.querySelector('body')
+const $mnuMobil = document.querySelector('#mnuMobil')
+
+$mnuMobil.addEventListener('click', () => {mnuBtn.click()})
 
 mnuBtn.addEventListener('click', function(){
-
-   // const img = document.createElement('img')
 
     if(mnuBtn.classList.contains('open')){ // close Hamburger menu
         mnuBtn.classList.remove('open')
@@ -25,3 +27,79 @@ mnuBtn.addEventListener('click', function(){
         
     }
 })
+
+// fix nav bar 
+
+window.addEventListener('scroll', ()=>{
+    const $navbar = document.getElementById('navbar')
+
+    if ($navbar.clientHeight < window.scrollY) {
+        $navbar.classList.add('navbar-effect')
+    } else{
+        $navbar.classList.remove('navbar-effect')
+    }
+
+})
+const options = {}
+
+
+// Intercepction
+
+$sections = document.querySelectorAll('section')
+$links = document.querySelectorAll('#navbarNavAltMarkup a')
+
+
+$interception = new IntersectionObserver((inputs, options)=>{
+    inputs.forEach(input => {
+        if(input.isIntersecting){
+            const id = '#' + input.target.id
+            history.pushState({},'',id)
+            $links.forEach(link => {
+                const href = link.attributes.href.nodeValue
+                if(href === id){
+                    link.classList.add('active')
+                } else {
+                    link.classList.remove('active')
+                }
+            })
+        }
+    });
+}, {
+    threshold: 1,
+    rootMargin: '0px 0px 30% 0px'
+})
+
+$sections.forEach(element => {
+    $interception.observe(element)
+});
+
+
+const $form = document.getElementById('contact-form')
+
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event){
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers:{
+            'Accept': 'application/json'
+        }
+    })
+    if(response.ok){
+        form.reset
+        handleAlert("Thanks for contact me, I'll contact you soon", 'success')
+    } else{
+        handleAlert("Something wrong happend, I couldn't send the enquiry", 'danger')
+    }
+}
+
+const $alertPlaceholder = document.getElementById('alertPlaceHolder')
+function handleAlert(message, type) {
+    var wrapper = document.createElement('div')
+    wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+  
+    $alertPlaceholder.append(wrapper)
+}
